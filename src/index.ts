7d540bd63c7037310d418b393b1b1fe3ea76e8eb
m0ar <edvard@hubinette.me>
2025-10-30 11:49:42 +0100

    chore: cleanup and adjust sentry config

--- src/index.ts
+++ src/index.ts
@@ -71,10 +71,6 @@ app.use(
     }),
 );
 
-app.get("/debug-sentry", function mainHandler(req, res) {
-    throw new Error("My first Sentry error!");
-});
-
 app.use("/api", api);
 
 app.get("/*", (req, res) =>
--- src/instrument.mjs
+++ src/instrument.mjs
@@ -3,20 +3,11 @@ import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 const dpidEnv = process.env.DPID_ENV;
 
-// Ensure to call this before importing any other modules!
 Sentry.init({
     dsn: "https://1f6d6a75201b566c085580cbb700ef69@o1330109.ingest.us.sentry.io/4510267031355392",
     environment: dpidEnv,
-    // Add Tracing by setting tracesSampleRate
-    // We recommend adjusting this value in production
-    tracesSampleRate: 1.0,
+    tracesSampleRate: 0.1,
     integrations: [nodeProfilingIntegration()],
-    // Set sampling rate for profiling - this is evaluated only once per SDK.init call
-    profileSessionSampleRate: 1.0,
-    // Trace lifecycle automatically enables profiling during active traces
+    profilesSampleRate: 0.1,
     profileLifecycle: "trace",
-
-    // Setting this option to true will send default PII data to Sentry.
-    // For example, automatic IP address collection on events
-    sendDefaultPii: true,
 });
